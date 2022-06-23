86c40ee7ca09affd0ab2cb2fa973a9c25277d585
Jesus Antonio <anton_jar@hotmail.com>
2022-06-23 07:15:03 -0500

    Adding initial files to branch logicStep1.js

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,63 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+//  We create the tile layer that will be the background of our map.
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//Create a base layer that hold both maps
+let baseMaps = {
+    'Streets': streets,
+    'Satellite Streets': satelliteStreets
+};
+
+//Create the map object with a center and zoom level
+let map = L.map('mapid', {
+    center: [39.5,-98.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+//Pass our map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map)
+
+// Accesing the GeoJSON data URL
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+// Create a style for the lines.
+let myStyle = {
+    color: "blue",
+    fillColor: "lightyellow",
+    opacity: 1,
+    fillOpacity:0.5,
+    weight: 1
+}
+
+//Grabbing our GeoJSON data
+d3.json(earthquakeData).then( function(data) {
+    console.log(data);
+    //Creating a GeoJSON layer with the retrieved data
+     L.geoJSON(data).addTo(map) //,{
+    //     style: myStyle,
+    //     onEachFeature: function(feature,layer){
+    //         layer.bindPopup("<h3>Neighborhood: "+feature.properties.AREA_NAME+"</h3>")
+    //     }
+    });
+//})
+  
+
+//To add popups to geoJSON https://leafletjs.com/reference.html#geojson
